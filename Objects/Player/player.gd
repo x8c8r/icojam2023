5a1a66decdc41e3977392a5a6f253a078a190c36
x8c8r <ex8c8r@gmail.com>
2023-07-29 02:53:47 +0600

    please do at least something

--- Objects/Player/player.gd
+++ Objects/Player/player.gd
@@ -6,10 +6,11 @@ enum player_state {
 	ATTACK
 }
 
-var current_state:player_state = player_state.IDLE
+var can_move:bool = false
 
-func _ready():
-	move(Vector2(64, 0))
+const MOVEMENT_SIZE = 64
+
+var current_state:player_state = player_state.IDLE
 
 func change_state(target_state:player_state) -> void:
 	current_state = target_state
@@ -17,10 +18,12 @@ func change_state(target_state:player_state) -> void:
 func get_current_state() -> player_state:
 	return current_state
 
-func move(increment:Vector2):
+func check_movement(increment:Vector2):
 	var tilemap:TileMap = get_tree().current_scene.get_node("Collision")
 	var pos_grid = tilemap.get_cell_tile_data(0, tilemap.local_to_map(position + increment))
-	if pos_grid: 
-		print("cant fucking move you dumb shit idiot")
+	if pos_grid: # There is a tile at that place
+		can_move = false
 	else:
-		print("can move :+1:")
+		can_move = true
+	
+	
